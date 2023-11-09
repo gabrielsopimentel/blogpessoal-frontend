9a8bf30d6f6686ec459e67e2dd9f99321bf61613
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-11-09 18:19:36 -0300

    posts perfil

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,14 +25,18 @@ function Navbar() {
           </Link>
 
           <div className="flex gap-4">
-            Postagens
+            <Link to="/postagens" className="hover:underline">
+              Postagens
+            </Link>
             <Link to="/temas" className="hover:underline">
               Temas
             </Link>
             <Link to="/cadastroTema" className="hover:underline">
               Cadastrar Tema
             </Link>
-            Perfil
+            <Link to="/perfil" className="hover:underline">
+              Perfil
+            </Link>
             <Link to="" onClick={logout} className="hover:underline">
               Sair
             </Link>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,18 @@
-import "./Home.css"
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+
 function Home() {
   return (
     <>
       <div className="bg-indigo-900 flex justify-center">
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">Seja bem vindo!</h2>
-            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
+            <h2 className="text-5xl font-bold">Seja bem-vindo(a)!</h2>
+            <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
 
             <div className="flex justify-around gap-4">
-              <div
-                className="rounded text-white 
-                                          border-white border-solid border-2 py-2 px-4"
-              >
-                Nova Postagem
+              <div className="flex justify-around gap-4">
+                <ModalPostagem />
               </div>
             </div>
           </div>
@@ -27,6 +26,8 @@ function Home() {
           </div>
         </div>
       </div>
+
+      <ListaPostagens />
     </>
   );
 }
